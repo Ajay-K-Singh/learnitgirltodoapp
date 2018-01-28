6b2ba6b2a576f69524b74b7bb34cc014ebb0a87d
Ajay Singh <aksingh1493@gmail.com>
2018-01-28 16:09:20 +0530

    Enhancements

--- app.js
+++ app.js
@@ -291,10 +291,6 @@ function displayTaskModal() {
     var modal = document.getElementById('myModal');
     $(modal).removeClass('remove-animatedly');
     modal.style.display = "block";
-    var span = document.getElementsByClassName("close")[0];
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
